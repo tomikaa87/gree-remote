62d3dbba2206c339f9f60e4f05e25b5ffb1d9678
tomikaa87 <tomikaa87@gmail.com>
2018-03-12 22:12:53 +0100

    GBB: clean up a little

--- GreeBlynkBridge/GreeBlynkBridge/Blynk/BlynkController.cs
+++ GreeBlynkBridge/GreeBlynkBridge/Blynk/BlynkController.cs
@@ -3,81 +3,10 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace GreeBlynkBridge.Blynk
 {
-    internal class PinAttribute : Attribute
-    {
-        public string DeviceParamName { get; private set; }
-        public bool IsBinary { get; set; }
-        public int ValueOffset { get; set; }
-        public bool IsReadOnly { get; set; }
-
-        public PinAttribute(string deviceParamName)
-        {
-            DeviceParamName = deviceParamName;
-        }
-    }
-
-    class PinConfiguration
-    {
-        public int deviceSelector;
-
-        [Pin(Gree.DeviceParameterKeys.Mode, ValueOffset = 1)]
-        public int setMode;
-
-        [Pin(Gree.DeviceParameterKeys.SetTemperature, IsReadOnly = true)]
-        public int getTemperature;
-
-        [Pin(Gree.DeviceParameterKeys.SetTemperature)]
-        public int setTemperature;
-
-        [Pin(Gree.DeviceParameterKeys.FanSpeed)]
-        public int setFanSpeed;
-
-        [Pin(Gree.DeviceParameterKeys.VerticalSwing, ValueOffset = 1)]
-        public int setVerticalSwing;
-
-        [Pin(Gree.DeviceParameterKeys.Power, IsBinary = true)]
-        public int setPower;
-
-        [Pin(Gree.DeviceParameterKeys.TurboMode, IsBinary = true)]
-        public int setTurbo;
-
-        [Pin(Gree.DeviceParameterKeys.QuietMode, IsBinary = true)]
-        public int setQuiet;
-
-        [Pin(Gree.DeviceParameterKeys.SleepMode, IsBinary = true)]
-        public int setSleep;
-
-        [Pin(Gree.DeviceParameterKeys.XfanMode, IsBinary = true)]
-        public int setXfan;
-
-        [Pin(Gree.DeviceParameterKeys.HealthMode, IsBinary = true)]
-        public int setHealth;
-
-        [Pin(Gree.DeviceParameterKeys.EnergySavingMode, IsBinary = true)]
-        public int setEnergySaving;
-
-        [Pin(Gree.DeviceParameterKeys.Light, IsBinary = true)]
-        public int setLight;
-
-        [Pin(Gree.DeviceParameterKeys.AirMode, IsBinary = true)]
-        public int setAir;
-
-        public override string ToString()
-        {
-            string s = $"\n\nPin configuration:\n";
-
-            foreach (var f in typeof(PinConfiguration).GetFields())
-                s += $"  {f.Name} = {f.GetValue(this)}\n";
-
-            return s;
-        }
-    }
-
     class BlynkController
     {
         private readonly BlynkLibrary.Blynk m_blynk;
---
+++ GreeBlynkBridge/GreeBlynkBridge/Blynk/PinAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GreeBlynkBridge.Blynk
+{
+    internal class PinAttribute : Attribute
+    {
+        public string DeviceParamName { get; private set; }
+        public bool IsBinary { get; set; }
+        public int ValueOffset { get; set; }
+        public bool IsReadOnly { get; set; }
+
+        public PinAttribute(string deviceParamName)
+        {
+            DeviceParamName = deviceParamName;
+        }
+    }
+}
