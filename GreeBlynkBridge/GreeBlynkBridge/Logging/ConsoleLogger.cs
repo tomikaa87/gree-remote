93c0848dbb2fd9fbc6bba5649512daede96555e6
tomikaa87 <tomikaa87@gmail.com>
2018-03-18 08:58:54 +0100

    GBB: add locking to ConsoleLogger

--- GreeBlynkBridge/GreeBlynkBridge/Logging/ConsoleLogger.cs
+++ GreeBlynkBridge/GreeBlynkBridge/Logging/ConsoleLogger.cs
@@ -7,6 +7,8 @@
     {
         private readonly string categoryName;
 
+        private readonly object consoleLockObject = new object();
+
         public ConsoleLogger(string categoryName)
         {
             this.categoryName = categoryName;
@@ -24,45 +26,48 @@
 
         public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
         {
-            if (exception != null)
+            lock (this.consoleLockObject)
             {
-                throw new NotImplementedException("Exception logging is not implemented");
+                if (exception != null)
+                {
+                    throw new NotImplementedException("Exception logging is not implemented");
+                }
+
+                var originalForegroundColor = Console.ForegroundColor;
+
+                Console.Write("[");
+
+                switch (logLevel)
+                {
+                    case LogLevel.Trace:
+                    case LogLevel.Debug:
+                    case LogLevel.None:
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        break;
+
+                    case LogLevel.Information:
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        break;
+
+                    case LogLevel.Warning:
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        break;
+
+                    case LogLevel.Error:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        break;
+
+                    case LogLevel.Critical:
+                        Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                        break;
+                }
+
+                Console.Write(logLevel.ToString()[0]);
+                Console.ForegroundColor = originalForegroundColor;
+                Console.Write("]");
+
+                Console.WriteLine($"[{this.categoryName}]: {state.ToString()}");
             }
-
-            var originalForegroundColor = Console.ForegroundColor;
-
-            Console.Write("[");
-
-            switch (logLevel)
-            {
-                case LogLevel.Trace:
-                case LogLevel.Debug:
-                case LogLevel.None:
-                    Console.ForegroundColor = ConsoleColor.Cyan;
-                    break;
-
-                case LogLevel.Information:
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    break;
-
-                case LogLevel.Warning:
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    break;
-
-                case LogLevel.Error:
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    break;
-
-                case LogLevel.Critical:
-                    Console.ForegroundColor = ConsoleColor.DarkMagenta;
-                    break;
-            }
-
-            Console.Write(logLevel.ToString()[0]);
-            Console.ForegroundColor = originalForegroundColor;
-            Console.Write("]");
-
-            Console.WriteLine($"[{this.categoryName}]: {state.ToString()}");
         }
     }
 }
